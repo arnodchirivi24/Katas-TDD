44f4e0246d2d33546e311cbde846e0019240426d
Arnod Chiriví <arnod.chirivi@sinco.com.co>
2025-10-17 21:16:23 -0500

    🟥 Creación de metodo ObtenerFizzBuzz

---
+++ Katas/FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+
+namespace Katas
+{
+    public class FizzBuzzTest
+    {
+        [Fact]
+        public void Debe_ObtenerFizzBuzz_DevolverLosNumerosDel_UnoAl_Cien()
+        {
+            var fizzBuzz = new FizzBuzz();
+
+            var resultadoNumerosUnoAlCien = fizzBuzz.ObtenerFizzBuzz();
+
+            resultadoNumerosUnoAlCien.Should().BeEquivalentTo(Enumerable.Range(1, 100).ToList());
+        }
+    }
+
+    internal class FizzBuzz
+    {
+        public FizzBuzz()
+        {
+        }
+
+        internal object ObtenerFizzBuzz()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
