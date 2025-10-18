1d7c250718256815aaaa8ae3745b0538b980ce54
Arnod Chiriví <arnod.chirivi@sinco.com.co>
2025-10-18 12:22:53 -0500

    🟦 Separación de test con la clase FizzBuzz

--- Katas/FizzBuzz/FizzBuzzTest.cs
+++ Katas/FizzBuzz/FizzBuzzTest.cs
@@ -1,6 +1,6 @@
 ﻿using FluentAssertions;
 
-namespace Katas
+namespace Katas.FizzBuzz
 {
     public class FizzBuzzTest
     {
@@ -112,48 +112,6 @@ namespace Katas
             var ultimoElemento = resultado.Count() - 1;
 
             resultado[ultimoElemento].Should().Be(palabraEsperada);
-        }
-    }
-
-    internal class FizzBuzzWhizz
-    {
-        private readonly Dictionary<int, string> _palabrasEspeciales = new Dictionary<int, string>
-        {
-           { 3, "Fizz" },
-           { 5, "Buzz" },
-           { 7, "Whizz" },
-           { 11, "Bang" }
-        };
-
-        private string _palabraDeSalida { get; }
-
-        public FizzBuzzWhizz()
-        {
-            _palabraDeSalida = string.Concat(_palabrasEspeciales.Values);
-        }
-
-        public List<object> ObtenerFizzBuzzWhizz()
-        {
-            List<object> resultadoFizzBuzz = new List<object>();
-
-            for(int i= 1; ; i++) 
-            {
-                string texto = ObtenerTextoFizzBuzzWhizz(i);
-                resultadoFizzBuzz.Add(string.IsNullOrEmpty(texto) ? i: texto);
-                if (texto == _palabraDeSalida) break;
-            }
-
-            return resultadoFizzBuzz;
-        }
-
-        private string ObtenerTextoFizzBuzzWhizz(int indice)
-        {    
-            string texto = "";
-
-            foreach(var item in _palabrasEspeciales)
-                if(indice % item.Key == 0) texto += item.Value;
-            
-            return texto;
         }
     }
 }
---
+++ Katas/FizzBuzz/FizzBuzzWhizz.cs
@@ -0,0 +1,44 @@
+﻿namespace Katas
+{
+    internal class FizzBuzzWhizz
+    {
+        private readonly Dictionary<int, string> _palabrasEspeciales = new Dictionary<int, string>
+        {
+           { 3, "Fizz" },
+           { 5, "Buzz" },
+           { 7, "Whizz" },
+           { 11, "Bang" }
+        };
+
+        private string _palabraDeSalida { get; }
+
+        public FizzBuzzWhizz()
+        {
+            _palabraDeSalida = string.Concat(_palabrasEspeciales.Values);
+        }
+
+        public List<object> ObtenerFizzBuzzWhizz()
+        {
+            List<object> resultadoFizzBuzz = new List<object>();
+
+            for(int i= 1; ; i++) 
+            {
+                string texto = ObtenerTextoFizzBuzzWhizz(i);
+                resultadoFizzBuzz.Add(string.IsNullOrEmpty(texto) ? i: texto);
+                if (texto == _palabraDeSalida) break;
+            }
+
+            return resultadoFizzBuzz;
+        }
+
+        private string ObtenerTextoFizzBuzzWhizz(int indice)
+        {    
+            string texto = "";
+
+            foreach(var item in _palabrasEspeciales)
+                if(indice % item.Key == 0) texto += item.Value;
+            
+            return texto;
+        }
+    }
+}
