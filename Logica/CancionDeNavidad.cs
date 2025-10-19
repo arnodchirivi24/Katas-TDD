b087170481215ccf8f33267cc27f4c646c535e61
Arnod Chiriví <arnod.chirivi@sinco.com.co>
2025-10-19 13:08:05 -0500

    🟦 Separacion de string y listados constantes a un nuevo archivo para dejar limpio el codigo de la logica

--- Logica/CancionDeNavidad.cs
+++ Logica/CancionDeNavidad.cs
@@ -3,41 +3,6 @@ namespace Logica
 {
     public class CancionDeNavidad
     {
-        private const string PimeraLinea = "On the [x] day of Christmas";
-        private const string SegundaLinea = "My true love sent to me:";
-        private static readonly string[] DiasOrdinales = new string[]
-        {
-                "first",
-                "second",
-                "third",
-                "fourth",
-                "fifth",
-                "sixth",
-                "seventh",
-                "eighth",
-                "ninth",
-                "tenth",
-                "eleventh",
-                "twelfth"
-        };
-
-        private static readonly string[] Regalos = new string[]
-        {
-            "A partridge in a pear tree.",
-            "Two turtle doves and",
-            "Three french hens",
-            "Four calling birds",
-            "Five golden rings",
-            "Six geese a-laying",
-            "Seven swans a-swimming",
-            "Eight maids a-milking",
-            "Nine ladies dancing",
-            "Ten lords a-leaping",
-            "Eleven pipers piping",
-            "Twelve drummers drumming"
-        };
-
-
         public List<List<string>> ObtenerCancion()
         {
            List<List<string>> estrofas = new List<List<string>>();
@@ -54,16 +19,20 @@ namespace Logica
         {
             List<string> estrofa = new List<string>();
 
-            string primeraLinea = PimeraLinea.Replace("[x]", DiasOrdinales[indiceEstrofa]);
-            estrofa.Add(primeraLinea);
-            estrofa.Add(SegundaLinea);
+            CrearPrimeraYSegundaLineaEstrofa(indiceEstrofa, estrofa);
 
-            for(int j= indiceEstrofa; j >=0; j--)
+            for (int j = indiceEstrofa; j >= 0; j--)
             {
-                estrofa.Add(Regalos[j]);
+                estrofa.Add(CancionDeNavidadHelpers.Regalos[j]);
             }
 
             return estrofa;
+        }
+
+        private static void CrearPrimeraYSegundaLineaEstrofa(int indiceEstrofa, List<string> estrofa)
+        {
+            estrofa.Add(CancionDeNavidadHelpers.PimeraLinea.Replace("[x]", CancionDeNavidadHelpers.DiasOrdinales[indiceEstrofa]));
+            estrofa.Add(CancionDeNavidadHelpers.SegundaLinea);
         }
     }
 }
---
+++ Logica/CancionDeNavidadHelpers.cs
@@ -0,0 +1,39 @@
+﻿namespace Logica
+{
+    internal static class CancionDeNavidadHelpers
+    {
+        public const string PimeraLinea = "On the [x] day of Christmas";
+        public const string SegundaLinea = "My true love sent to me:";
+        public static readonly string[] DiasOrdinales = new string[]
+        {
+                "first",
+                "second",
+                "third",
+                "fourth",
+                "fifth",
+                "sixth",
+                "seventh",
+                "eighth",
+                "ninth",
+                "tenth",
+                "eleventh",
+                "twelfth"
+        };
+
+        public static readonly string[] Regalos = new string[]
+        {
+            "A partridge in a pear tree.",
+            "Two turtle doves and",
+            "Three french hens",
+            "Four calling birds",
+            "Five golden rings",
+            "Six geese a-laying",
+            "Seven swans a-swimming",
+            "Eight maids a-milking",
+            "Nine ladies dancing",
+            "Ten lords a-leaping",
+            "Eleven pipers piping",
+            "Twelve drummers drumming"
+        };
+    }
+}
