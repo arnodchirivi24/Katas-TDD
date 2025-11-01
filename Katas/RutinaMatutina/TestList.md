a8e07179306ad61ab717c99b2acf198d750ca91d
Arnod Chiriví <arnod.chirivi@sinco.com.co>
2025-11-01 16:24:08 -0500

    🟩 Se obtiene la tarea  correspondiente

--- Katas/RutinaMatutina/TestList.md
+++ Katas/RutinaMatutina/TestList.md
@@ -1,7 +1,6 @@
-[ ] Debe obtener la tarea Tiempo libre el metodo QueDeboHacerAhora
-[ ] Debe poder agregar tareas en horas especificas entre una fecha hora inicial y final en UTC
-[ ] Debe el metodo QueDeboHacerAhora otener la tarea "Hacer ejercicio" dentro de la hora 06:00 a 06:59
+[x] Debe obtener la tarea Tiempo libre el metodo QueDeboHacerAhora
+[x] Debe poder agregar tareas en horas especificas entre una fecha hora inicial y final
+[x] Debe el metodo QueDeboHacerAhora otener la tarea "Hacer ejercicio" dentro de la hora 06:00 a 06:59
 [ ] Debe obtener la tarea Leer y estudiar cuando las horas son entre 07:00 a 07:59 
 [ ] Debe obtener la tarea Desayunar cuando las horas son entre 08:00 a 08:59  
-[ ] Debe obtener No tiene tarea asignada si dentro de las horas no se encuentra alguna
 [ ] Debe validarse que no se pueden agregar tareas si la fecha hora inicial y final estan dentro del rango de las horas existentes
--- Logica/RutinaMatutina.cs
+++ Logica/RutinaMatutina.cs
@@ -3,18 +3,38 @@ namespace Logica
 {
     public class RutinaMatutina: IRutinaMatutina
     {
-        public RutinaMatutina()
-        {
-        }
+        private List<Tarea> _tareas = new List<Tarea>();
 
         public void AgregarTarea(DateTime inicio, DateTime fin, string tarea)
         {
-        
+             Tarea nuevaTarea = new Tarea(inicio, fin, tarea);
+             _tareas.Add(nuevaTarea);
         }
 
-        public string QueDeboHacerAhora(DateTime horaActual)
+        public string QueDeboHacerAhora(DateTime fechaYHoraActual)
         {
+
+            var tareaActual = _tareas.SingleOrDefault(tarea => fechaYHoraActual >= tarea.Inicio && fechaYHoraActual < tarea.Fin);
+            
+            if (tareaActual != null)
+            {
+                return tareaActual.Descripcion;
+            }
+            
             return "Tiempo libre";
+        }
+    }
+
+    public class Tarea
+    {
+        public DateTime Inicio { get; }
+        public DateTime Fin { get; }
+        public string Descripcion { get; }
+        public Tarea(DateTime inicio, DateTime fin, string descripcion)
+        {
+            Inicio = inicio;
+            Fin= fin;
+            Descripcion = descripcion;
         }
     }
 }
