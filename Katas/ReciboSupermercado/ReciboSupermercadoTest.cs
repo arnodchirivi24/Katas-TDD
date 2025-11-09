2d87e565a9f5d2ec22d0e070605ad07f9cd85930
Arnod Chiriví <arnod.chirivi@sinco.com.co>
2025-11-08 19:20:15 -0500

    🟥 Debe_CalcularCostoTotal_CuandoSeCompran2CepillosConPromocionDe2x1YPrecioDe0_99CadaUno_DevuelveTotalDe1_98_Euros

--- Katas/ReciboSupermercado/ReciboSupermercadoTest.cs
+++ Katas/ReciboSupermercado/ReciboSupermercadoTest.cs
@@ -12,5 +12,19 @@ namespace Katas.ReciboSupermercadoTest
 
             reciboSuper.CalcularCostoTotal(2, 0.99m, "Cepillo").Should().Be(costoTotal);
         }
+
+        [Fact]
+        public void Debe_CalcularCostoTotal_CuandoSeCompran2CepillosConPromocionDe2x1YPrecioDe0_99CadaUno_DevuelveTotalDe1_98_Euros()
+        {
+            //Arrange
+            var unidades = 3;
+            var valorUnidad = 0.99m;
+            var costoTotal = 1.98m;
+            var reciboSuper = new ReciboSupermercado();
+            //Act
+
+            //Assert
+            reciboSuper.CalcularCostoTotal(unidades, valorUnidad, "Cepillo").Should().Be(costoTotal);
+        }
     }
 }
