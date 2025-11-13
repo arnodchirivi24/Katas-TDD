5f7228a515b39432554e170d4c92e99edaa88b30
Arnod Chiriví <arnod.chirivi@sinco.com.co>
2025-11-13 18:50:53 -0500

    🟥  Debe_WrapDevolverUnTextoVacioSiSuParametroDeTextoEsVacio

---
+++ Katas/WordWrap/WordWrapTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+namespace Katas.WordWrap.Tests;
+
+public class WordWrapTests
+{
+    [Fact]
+    public void Debe_WrapDevolverUnTextoVacioSiSuParametroDeTextoEsVacio()
+    {
+        var result = Wrap("", 1);
+
+        result.Should().Be("");
+    }
+
+    private string Wrap(string texto, int numeroColumnas)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+
